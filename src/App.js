a187616e63e9d07d4ae928f31de1c74bda0c0826
Angus Kellett <anguskellett@hotmail.com>
2021-05-18 17:33:52 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to my React App
+      </h1>
+      <h3>
+        Here you will find just a bunch of trial and error React business
+      </h3>
+    </div>
+  );
+}
+
+export default App;
